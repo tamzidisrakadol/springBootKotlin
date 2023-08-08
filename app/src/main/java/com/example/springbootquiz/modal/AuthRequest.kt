8f6661b6f9700d8964cce470756245ce3b75e47e
Tamzid Israk <tamzidisrak12@gmail.com>
2023-08-08 23:55:41 +0600

    build a quiz app using spring boot as backend
    showing question and checking answer
    
    successfully jwt authentication with spring boot

--- app/src/main/java/com/example/springbootquiz/modal/AuthRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.springbootquiz.modal
-
-data class AuthRequest (
-    var email:String,
-    var password:String
-)
--- app/src/main/java/com/example/springbootquiz/modal/LoginResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.example.springbootquiz.modal
-
-data class LoginResponse(
-    var jwtToken:String,
-    var userName:String
-)
